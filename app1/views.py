2e6b7dde18ffdab3279fa32f72a2401bc7fd898d
poornimakandukoori <poornimakandukuri2610@gmail.com>
2023-11-21 21:02:37 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def janu(request):
+     return render(request,'janu.html') 
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sree(request):
+    return render(request,'sree.html')
